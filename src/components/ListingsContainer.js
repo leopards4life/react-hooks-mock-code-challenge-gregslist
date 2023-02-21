e08520228e6e3f54be8cdbdc1c76373dd59f210e
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-02-21 13:22:16 -0700

    Add favorite functionality

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:6001/listings")
+      .then(res => res.json())
+      .then(listings => setListings(listings))
+    }, [])
+
+    const listItems = listings.map(listing => (
+      <ListingCard 
+      key={listing.id}
+      description={listing.description}
+      image={listing.image}
+      location={listing.location}
+      />
+    ));
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listItems}
       </ul>
     </main>
   );
